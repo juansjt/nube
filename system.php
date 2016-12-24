b2da9a15ba0d072ef4e13e8493925aeb906092ae
juansjt <juan.sjt@gmail.com>
2016-12-23 20:45:34 -0500

    Update system.php

--- system.php
+++ system.php
@@ -1,7 +1,7 @@
 <?php
 
 function MySQLQuery($query){
-$conexion = mysql_connect("localhost", "root", ""); // url, user, pass
+$conexion = mysql_connect(getenv("OPENSHIFT_MYSQL_DB_HOST"), getenv("OPENSHIFT_MYSQL_DB_USERNAME"), getenv("OPENSHIFT_MYSQL_DB_PASSWORD"));
 mysql_select_db("agenda", $conexion);
 $result = mysql_query($query, $conexion);
 mysql_close($conexion);
